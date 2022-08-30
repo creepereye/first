8dfe409084b23cfc17ba05d9089d40bb554ba5d9
creepereye <70457927+creepereye@users.noreply.github.com>
2022-08-30 21:45:30 +0900

    Add files via upload

---
+++ static/js/Test.js
@@ -0,0 +1,40 @@
+// var a=""
+// fetch("https://creepereyeproxy.herokuapp.com/https://www.op.gg/summoners/kr/l%ED%94%BC%EC%B9%B4%EC%B8%84l")
+//   .then((response) => response)
+//   .then((data) => a=data);
+function Init_Text_1(e){
+    
+    
+ 
+    if(e.keyCode==13){
+        document.getElementsByClassName("input_1")[0].value=""
+        
+    }
+}
+
+
+function Enter_1(e){
+    
+    console.log('ddddd')
+    document.getElementsByClassName("input_1")[0].value=""
+    
+}
+
+
+function Init_Text_2(e){
+    
+    
+    
+    if(e.keyCode==13){
+        document.getElementsByClassName("input_2")[0].value=""
+        
+    }
+}
+
+
+function Enter_2(e){
+    
+    console.log('ddddd')
+    document.getElementsByClassName("input_2")[0].value=""
+    
+}
